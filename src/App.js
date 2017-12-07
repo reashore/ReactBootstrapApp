74a32f4c275472a4ee0b7770dfceefb9a7eb73ed
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-12-07 11:54:46 -0800

    Saving

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, ButtonToolbar, Grid, Row, Col, Navbar, NavItem, MenuItem, NavDropdown, Nav, } from 'react-bootstrap';
+
+function handleSelect(selectedKey) {
+  alert(`selected ${selectedKey}`);
+}
+
+const TopMenu = () =>
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">React-Bootstrap</a>
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Nav onSelect={handleSelect}>
+      <NavItem eventKey={1} href="#">Link</NavItem>
+      <NavItem eventKey={2} href="#">Link</NavItem>
+      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+        <MenuItem eventKey={3.1}>Action</MenuItem>
+        <MenuItem eventKey={3.2}>Another action</MenuItem>
+        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey={3.4}>Separated link</MenuItem>
+      </NavDropdown>
+    </Nav>
+  </Navbar>;
+
+const ButtonBar = () =>
+  <ButtonToolbar>
+    <Button>Default</Button>
+    <Button bsStyle="primary">Primary</Button>
+    <Button bsStyle="success">Success</Button>
+    <Button bsStyle="info">Info</Button>
+    <Button bsStyle="warning">Warning</Button>
+    <Button bsStyle="danger">Danger</Button>
+    <Button bsStyle="link">Link</Button>
+  </ButtonToolbar>;
+
+const CustomGrid = () =>
+  <Grid>
+    <Row className="show-grid">
+      <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
+      <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+    </Row>
+    <Row className="show-grid">
+      <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+      <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+      <Col xsHidden md={4}><code>&lt;{'Col xsHidden md={4}'} /&gt;</code></Col>
+    </Row>
+    <Row className="show-grid">
+      <Col xs={6} xsOffset={6}><code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code></Col>
+    </Row>
+    <Row className="show-grid">
+      <Col md={6} mdPush={6}><code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code></Col>
+      <Col md={6} mdPull={6}><code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code></Col>
+    </Row>
+  </Grid>;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <TopMenu />
+        <h1>React Bootstrap App</h1>
+        <ButtonBar />
+        <CustomGrid />
       </div>
     );
   }
