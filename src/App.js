8d5fc5b0a9b72561024a0a6906edd218cf40b60c
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-12-08 10:50:18 -0800

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -41,9 +41,7 @@ const CustomJumbotron = () =>
     <h1>React Bootstrap App</h1>
     <p>A simple jumbotron-style component for calling extra attention to featured content or information.</p>
     <p><Button bsStyle="primary">Learn more</Button></p>
-  </Jumbotron>
-;
-
+  </Jumbotron>;
 
 const CustomButtonBar = () =>
   <ButtonToolbar>
